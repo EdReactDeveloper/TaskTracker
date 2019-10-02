0f41f5da36c3604e2cac30b303a4360ec8887689
EdReactDeveloper <edinbox30@gmail.com>
2019-10-02 16:43:10 +0300

    react createPortal for the modal

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import style from './Modal.module.scss';
+import ReactDOM from 'react-dom';
 
-const Modal = ({ modalHandler, modalType, children }) => { // rendered by containers/Boards.jsx
-  return (
-    <>
-      <div className={style.background} onClick={() => modalHandler(modalType)}></div>
-      <div className={style.content}>
-       {children}
-      </div>
-    </>
-  );
-};
+class Modal extends Component {
+  componentWillMount() {
+    this.root = document.createElement('div')
+    document.body.appendChild(this.root)
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.root)
+  }
+  render() {
+
+    const { modalHandler, modalType, children } = this.props // rendered by containers/Boards.jsx
+    return ReactDOM.createPortal(
+      <>
+        <div className={style.background} onClick={() => modalHandler(modalType)}></div>
+        <div className={style.content}>
+          {children}
+        </div>
+      </>, this.root
+    );
+  }
+}
 
 export default Modal;
