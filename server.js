760e55c762ca9370de6a90b05afe38d124a1abd2
EdReactDeveloper <edinbox30@gmail.com>
2019-10-06 22:38:37 +0300

    refactoring

--- server.js
+++ server.js
@@ -58,13 +58,12 @@ app.use('/api/topics', require('./routes/api/topics'));
 // serve static assets in production 
 if(process.env.NODE_ENV === 'production'){
   // set static folder 
-  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  app.use(express.static('client/build'))
 
   app.get('*', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=> console.log(`app is runninig at port ${PORT}`))
