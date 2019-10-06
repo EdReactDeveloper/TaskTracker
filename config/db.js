868d1f43e3de3b987fbf8cbc5054d3cc02160292
EdReactDeveloper <edinbox30@gmail.com>
2019-10-06 23:26:49 +0300

    useUnifiedTopology: true

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const config = require('config')
+
 const db = config.get('mongoURI')
 
 const connectDB = async()=>{
   try {
-    await mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
+    await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
   } catch (error) {
     console.error(error.message)
     process.exit(1)
